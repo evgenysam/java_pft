90702d6199136ceba6cd672208bc76ff38a5264e
evgenysam <evgenysam@gmail.com>
2016-05-29 00:41:02 -0400

    added functions on square and rectangal

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -3,7 +3,25 @@ package ru.stqa.pft.sandbox;
 public class MyFirstProgram {
 
   public static void main(String[] args) {
-   System.out.println("Hello, world!");
-   }
+    hello("world");
+    hello("user");
+    hello("evgeny");
+    double l = 5;
+    System.out.println("Area of square with side" + l + " = " + area(l));
+    double a = 4;
+    double b = 6;
+    System.out.println("Area of rectangal with sides " + a + " and " + b + " = " + area(a,b));
+  }
 
+  public static void hello(String somebody) {
+    System.out.println("Hello, " + somebody + "!");
+  }
+
+  public static double area(double l){
+    return l * l;
+  }
+
+  public static double area(double a, double b){
+    return a * b;
+  }
 }
