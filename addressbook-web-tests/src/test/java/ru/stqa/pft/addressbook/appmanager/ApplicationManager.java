1f38ce23b23eaece9779205a7b6b230ab1163034
evgenysam <evgenysam@gmail.com>
2016-06-01 11:56:37 -0400

    class2 video 11
    class2 video 11

---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -0,0 +1,39 @@
+package ru.stqa.pft.addressbook.appmanager;
+
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Created by esamokhodkin on 5/30/2016.
+ */
+public class ApplicationManager {
+  FirefoxDriver wd;
+  private NavigationHelper navigationHelper;
+  private GroupHelper groupHelper;
+  private SessionHelper sessionHelper;
+
+  public void init() {
+    wd = new FirefoxDriver();
+    wd.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
+    wd.get("http://localhost:8080/addressbook/");
+    groupHelper = new GroupHelper(wd);
+    navigationHelper = new NavigationHelper(wd);
+    sessionHelper.login("admin", "secret");
+    sessionHelper = new SessionHelper(wd);
+  }
+
+
+
+  public void stop() {
+    wd.quit();
+  }
+
+  public GroupHelper getGroupHelper() {
+    return groupHelper;
+  }
+
+  public NavigationHelper getNavigationHelper() {
+    return navigationHelper;
+  }
+}
